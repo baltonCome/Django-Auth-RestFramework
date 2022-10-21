dc9fe8f5d922747e2efd50b59c5a84f3c457fa1c
Baltoncome <Baltoncome@gmail.com>
2022-10-21 13:52:30 +0200

    CRUD

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path("homepage", views.homepage, name="posts_home"),
-    path("list_posts", views.list_posts, name="list_posts"),
-    path("list_posts/<int:post_id>", views.post_details, name="post_details")
+    path("", views.list_posts, name="list_posts"),
+    path("<int:post_id>", views.post_details, name="post_details"),
+    path("update/<int:post_id>", views.update_post , name="update_post"),
+    path("delete/<int:post_id>", views.delete_post , name="delete_post")
 ] 
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,16 @@
+from email import message
 from urllib import response
 from django.shortcuts import render
+from django.urls import is_valid_path
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from . models import Post
+from . serializers import PostSerializer
+from django.shortcuts import get_object_or_404
+
+from posts import serializers
 
 # Create your views here.
 
@@ -18,12 +25,70 @@ def homepage(request:Request):
     return Response(data=response, status=status.HTTP_200_OK) 
 
 
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "POST"])
 def list_posts(request:Request):
-    return Response()
+
+    if request.method == "POST":
+        data = request.data
+        serializer = PostSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            response = {
+                "message" : "Post Created",
+                "data" : serializer.data
+            }
+            return Response(data=response, status=status.HTTP_201_CREATED)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    posts = Post.objects.all()
+    serializer = PostSerializer(instance=posts, many = True)
+    response = {
+        "message" : "Posts",
+        "data" : serializer.data
+    }
+    return Response(data=response, status=status.HTTP_200_OK)
 
 
 
 @api_view(http_method_names=["GET"])
-def post_details(request:Request, post_index:int):
-    return Response()
+def post_details(request:Request, post_id:int):
+    post = get_object_or_404(Post, pk=post_id)
+
+    serializer = PostSerializer(instance=post)
+
+    response = {
+        "message" : "Post Details",
+        "data" : serializer.data
+    }
+    return Response(data= response,status=status.HTTP_200_OK)
+
+
+@api_view(http_method_names=["PUT"])
+def update_post(request:Request, post_id:int):
+    post = get_object_or_404(Post, pk=post_id)
+
+    data = request.data
+
+    serializer = PostSerializer(instance=post, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+        response = {
+            "message" : "Post Updated",
+            "data" : serializer.data
+        }
+        return Response(data=response, status=status.HTTP_200_OK)
+    return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+@api_view(http_method_names=["DELETE"])
+def delete_post(request:Request, post_id:int):
+    post = get_object_or_404(Post, pk=post_id)
+    serializer = PostSerializer(instance=post)   
+    post.delete()
+    response = {
+        "message" : "Post Deleted",
+        "data" : serializer.data
+    }
+    return Response(data= response, status = status.HTTP_204_NO_CONTENT)
