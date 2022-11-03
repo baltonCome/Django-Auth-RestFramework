f6c42c569e43e103515f44ebb6f75848eb2eb9b1
Baltoncome <Baltoncome@gmail.com>
2022-11-03 11:10:25 +0200

    Serializer relashioship

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,9 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
+from . import views
+from django.urls import path
+
 urlpatterns = [
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("login/", views.LoginView.as_view(), name="login"),
--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,20 @@
 from statistics import mode
 from turtle import title
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+
+User = get_user_model()
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    link = models.TextField(blank=True)
+    proceure = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add = True)
+    author = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "posts")
 
     def __str__(self) -> str:
         return self.title
